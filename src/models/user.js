61f3556d142b1b5b6d3e2b9c078ac56c288346c9
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2022-03-20 22:19:54 +0300

    jwt and tokens added

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,7 +40,13 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a postive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -55,6 +62,15 @@ userSchema.statics.findByCredentials = async (email, password) => {
         throw new Error('Unable to login!')
     }
     return user
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString() }, 'thisismynewcourse')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+
+    return token
 }
 
 userSchema.pre('save', async function (next) {
