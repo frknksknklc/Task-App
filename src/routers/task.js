40705e8d3506969389e11ed59c85be91670bfd64
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2022-03-26 17:17:30 +0300

    User-Task Relationship

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
@@ -25,9 +26,11 @@ router.get('/tasks/:id', async (req,res) => {
     }
 })
 
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
     try{
         await task.save()
         res.status(201).send(task)
